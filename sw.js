296da0ae3a1aa4177ad5e4978d28cb70491fe2d1
liuw <liuw@yl-scm.com>
2021-01-06 15:33:34 +0800

    010608

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
 var cacheStorageKey = 'minimal-pwa-5'
 var cacheList=[
-  '/PWA/',
+  '/PWA',
   '/PWA/index.html',
   '/PWA/main.css',
   '/PWA/1.png'
