84b925a8834b99f66cbb37f4b6cfed1ecbee05af
liuw <liuw@yl-scm.com>
2021-01-06 15:11:32 +0800

    010605

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
 var cacheStorageKey = 'minimal-pwa-2'
 var cacheList=[
-  '/',
-  'index.html',
-  'main.css',
-  '1.png'
+  'PWA/',
+  'PWA/index.html',
+  'PWA/main.css',
+  'PWA/1.png'
 ]
 self.addEventListener('install',e =>{
   e.waitUntil(
